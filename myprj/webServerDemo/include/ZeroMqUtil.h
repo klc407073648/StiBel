3fd744307f36ed58bf46613ec6ba6837b0499c75
klc407073648 <407073648@qq.com>
2021-10-05 19:43:51 +0800

    ZeroMqUtil方法添加注释

--- myprj/webServerDemo/include/ZeroMqUtil.h
+++ myprj/webServerDemo/include/ZeroMqUtil.h
@@ -8,25 +8,31 @@
 
 #ifndef __STIBEL_ZeroMqUtil_H__
 #define __STIBEL_ZeroMqUtil_H__
-#include<string>
+#include <string>
 
 //使用多种协议，inproc（进程内）、ipc（进程间）、tcp、pgm（广播）、epgm；
-enum ZeroMqProtocol {
-		INPROC,
-		IPC,
-		TCP,
-		PGM,
-    	EPGM
-	};
+enum ZeroMqProtocol
+{
+	INPROC,
+	IPC,
+	TCP,
+	PGM,
+	EPGM
+};
 
 class ZeroMqUtil
 {
 public:
-  ZeroMqUtil();
-  ~ZeroMqUtil();
-  static std::string sendZeroMqMessage(ZeroMqProtocol protocol, const std::string &targetObj, const std::string &sendContent, int timeOut=0);
+	ZeroMqUtil();
+	~ZeroMqUtil();
+	/**
+     * @brief  通过zeromq发送消息.
+     *
+     * @return 接收targetObj返回的消息字符串
+     */
+	static std::string sendZeroMqMessage(ZeroMqProtocol protocol, const std::string &targetObj, const std::string &sendContent, int timeOut = 0);
+
 private:
-  
 };
 
 #endif //__STIBEL_ZeroMqUtil_H__
